1df7e80a08ed7a58035ae3a8b8af8d582864a03d
mmorkos-cyber <mariomorkos98@gmail.com>
2026-04-13 23:56:38 +0200

    started with the projects and added a form api

--- Script/script.js
+++ Script/script.js
@@ -6,3 +6,42 @@ const observer = new IntersectionObserver(entries => {
             });
             }, { threshold: 0.1 });
         document.querySelectorAll('.fade-up').forEach(el => observer.observe(el));
+
+
+// FORMSPREE
+
+window.formspree = window.formspree || function () {
+  (formspree.q = formspree.q || []).push(arguments);
+};
+
+formspree("initForm", {
+  formElement: "#contact-form",
+  formId: "xojyajqr",
+  disableAutoMessages: true
+});
+
+
+
+// Reset form after success
+document.addEventListener("formspree:success", () => {
+  const form = document.querySelector("#contact-form");
+  if (form) form.reset();
+});
+
+
+//success message
+document.addEventListener("formspree:success", () => {
+  const successBox = document.querySelector("[data-fs-success]");
+  if (successBox) {
+    successBox.textContent = "✓ Message envoyé avec succès !";
+  }
+});
+
+
+//error message
+document.addEventListener("formspree:error", () => {
+  const errorBox = document.querySelector("[data-fs-error]");
+  if (errorBox) {
+    errorBox.textContent = "Une erreur est survenue. Veuillez réessayer.";
+  }
+});
